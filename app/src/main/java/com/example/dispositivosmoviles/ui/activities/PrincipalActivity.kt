f8d2844dfd7690c5db7f94944b939a7440da05c9
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-06 19:37:18 -0500

    navigation bar

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
@@ -7,13 +7,14 @@ import android.util.Log
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
 import com.example.dispositivosmoviles.databinding.PrincipalActivityBinding
+import com.google.android.material.snackbar.Snackbar
 
 class PrincipalActivity : AppCompatActivity() {
     private lateinit var binding: PrincipalActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("UCE","Entrando a Create")
+        Log.d("UCE", "Entrando a Create")
         binding = PrincipalActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -29,20 +30,56 @@ class PrincipalActivity : AppCompatActivity() {
 //        }
 
         //opcion 2
-        var name:String=""
-        intent.extras.let {
+        var name: String = ""
+        /*intent.extras.let {
             name = it?.getString("var1")!!
-        }
-        Log.d("UCE","Hola ${name}")
+        }*/
+        Log.d("UCE", "Hola ${name}")
         binding.txtName.text = "Bienvenido " + name.toString()
-        Log.d("UCE","Entrando a Start")
+        Log.d("UCE", "Entrando a Start")
         initClass()
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.inicio -> {
+
+                    var suma:Int = 0
+                    for(i in listOf(1,2,3)){
+                        suma +=i
+                    }
+                    Snackbar.make(binding.txtName,
+                        "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG)
+                        .show()
+                    true
+                }
+
+                R.id.favoritos -> {
+                    var suma:Int = 0
+                    for(i in listOf(8,12,13)){
+                        suma +=i
+                    }
+                    Snackbar.make(binding.txtName,
+                        "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG)
+                        .show()
+                    true
+                }
+
+                R.id.chatgpt -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+
+                else -> false
+            }
+        }
 
     }
 
     private fun initClass() {
         binding.botonRetorno.setOnClickListener {
-             //el primer parametro es un filtro para que muestre solo eso en la consola al filtar por el termino y el segundo el mensaje
+            //el primer parametro es un filtro para que muestre solo eso en la consola al filtar por el termino y el segundo el mensaje
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
