8e6dd0579e81e87faadc4ba6b32e89e75c7150b2
Pablo Suntaxi <pbsun.ale@gmail.com>
2023-06-12 23:59:19 -0500

    clase 12/06/23: Spinners

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
 import com.example.dispositivosmoviles.databinding.PrincipalActivityBinding
 import com.example.dispositivosmoviles.ui.fragments.FavoritesFragment
 import com.example.dispositivosmoviles.ui.fragments.FirstFragment
 import com.example.dispositivosmoviles.ui.fragments.ChatGptFragment
-import com.google.android.material.snackbar.Snackbar
+import com.example.dispositivosmoviles.utilities.FragmentsManager
 
 class PrincipalActivity : AppCompatActivity() {
     private lateinit var binding: PrincipalActivityBinding
@@ -46,34 +47,41 @@ class PrincipalActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.inicio -> {
 
-                    val frag = FirstFragment() //crea una instancia del fragment que se quiere agregar
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
-                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
-                    transaction.addToBackStack(null)
-                    transaction.commit() //guarda los cambios
+//                    val frag = FirstFragment() //crea una instancia del fragment que se quiere agregar
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
+//                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
+//                    transaction.addToBackStack(null)
+//                    transaction.commit() //guarda los cambios
 
+                    FragmentsManager().replaceFragmet(supportFragmentManager,
+                        binding.frmContainer.id, FirstFragment())
                     true
                 }
 
                 R.id.favoritos -> {
-                    val frag = FavoritesFragment() //crea una instancia del fragment que se quiere agregar
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
-                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
-                    transaction.addToBackStack(null)
-                    transaction.commit() //guarda los cambios
+//                    val frag = FavoritesFragment() //crea una instancia del fragment que se quiere agregar
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
+//                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
+//                    transaction.addToBackStack(null)
+//                    transaction.commit() //guarda los cambios
 
+                    FragmentsManager().replaceFragmet(supportFragmentManager,
+                        binding.frmContainer.id, FavoritesFragment())
                     true
                 }
 
                 R.id.chatgpt -> {
-                    val frag = ChatGptFragment() //crea una instancia del fragment que se quiere agregar
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
-                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
-                    transaction.addToBackStack(null)
-                    transaction.commit() //guarda los cambios
+//                    val frag = ChatGptFragment() //crea una instancia del fragment que se quiere agregar
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
+//                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
+//                    transaction.addToBackStack(null)
+//                    transaction.commit() //guarda los cambios
+
+                    FragmentsManager().replaceFragmet(supportFragmentManager,
+                        binding.frmContainer.id, ChatGptFragment())
                     true
                 }
 
@@ -81,6 +89,9 @@ class PrincipalActivity : AppCompatActivity() {
             }
         }
 
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 
     private fun initClass() {
--- app/src/main/java/com/example/dispositivosmoviles/ui/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/fragments/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.FragmentFirstBinding
 
@@ -20,6 +21,20 @@ class FirstFragment : Fragment() {
 
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val names = arrayListOf<String>(
+            "Carlos", "Xavier", "Andres",
+            "Pepe", "Mariano", "Rosa"
+        )
+        val adapter =
+            ArrayAdapter<String>(requireActivity(),
+
+                R.layout.spinner_item_layaout, names)
+        binding.spinner.adapter = adapter
+        binding.listview.adapter = adapter
     }
 
 
