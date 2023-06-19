a349f66ab7c92ac6e6c897b06ff646c197641e01
Pablo Suntaxi <pbsun.ale@gmail.com>
2023-06-19 12:34:44 -0500

    clase 17/06/23

--- app/src/main/java/com/example/dispositivosmoviles/ui/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/fragments/FirstFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.FragmentFirstBinding
+import com.example.dispositivosmoviles.logic.list.ListItems
+import com.example.dispositivosmoviles.ui.adapters.MarvelAdapter
 
 class FirstFragment : Fragment() {
     private lateinit var binding: FragmentFirstBinding
@@ -29,12 +33,21 @@ class FirstFragment : Fragment() {
             "Carlos", "Xavier", "Andres",
             "Pepe", "Mariano", "Rosa"
         )
-        val adapter =
-            ArrayAdapter<String>(requireActivity(),
 
-                R.layout.spinner_item_layaout, names)
+        val adapter = ArrayAdapter<String>(requireActivity(), R.layout.simple_layout, names)
+
         binding.spinner.adapter = adapter
-        binding.listview.adapter = adapter
+        val rvAdapter = MarvelAdapter(ListItems().returnMarvelChars())
+
+        val rvMArvel = binding.rvMarvelChars
+        rvMArvel.adapter = rvAdapter
+
+        rvMArvel.layoutManager = LinearLayoutManager(
+            requireActivity(), LinearLayoutManager.VERTICAL,
+            false
+        )
+
+
     }
 
 
